491db55cdc116c75295b61e2a441af89b37aca85
lemoncmd <lemoncmd@gmail.com>
2024-09-15 20:33:32 +0900

    create rust wrappers

---
+++ build.rs
@@ -0,0 +1,17 @@
+use std::{env, path::Path};
+
+fn main() {
+    let dst = cmake::build("mlir");
+    println!("cargo:rustc-link-search=native={}", dst.display());
+    println!("cargo:rustc-link-lib=static=SampleCAPI");
+    println!("cargo:rustc-link-lib=static=MLIRSample");
+    println!("cargo:rerun-if-changed=mlir");
+
+    bindgen::builder()
+        .header("mlir/include/sample-c/Dialects.h")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        .generate()
+        .unwrap()
+        .write_to_file(Path::new(&env::var("OUT_DIR").unwrap()).join("bindings.rs"))
+        .unwrap();
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,77 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+mod import {
+    #![allow(non_upper_case_globals)]
+    #![allow(non_camel_case_types)]
+    #![allow(non_snake_case)]
+    #![allow(unused)]
+    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+}
+
+use melior::dialect::DialectHandle;
+
+pub fn sample() -> DialectHandle {
+    unsafe {
+        DialectHandle::from_raw(mlir_sys::MlirDialectHandle {
+            ptr: import::mlirGetDialectHandle__sample__().ptr,
+        })
+    }
+}
+
+melior::dialect! {
+    name: "sample",
+    table_gen: r#"include "mlir/include/sample/SampleOps.td""#,
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use melior::{
+        dialect::{func, DialectRegistry},
+        ir::{
+            attribute::{StringAttribute, TypeAttribute},
+            r#type::FunctionType,
+            Block, Location, Module, Region,
+        },
+        utility::register_all_dialects,
+        Context,
+    };
+
+    use crate::sample;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn dialect() {
+        let registry = DialectRegistry::new();
+        register_all_dialects(&registry);
+        sample().insert_dialect(&registry);
+
+        let context = Context::new();
+        context.append_dialect_registry(&registry);
+        context.load_all_available_dialects();
+
+        let location = Location::unknown(&context);
+        let module = Module::new(location);
+
+        module.body().append_operation(func::func(
+            &context,
+            StringAttribute::new(&context, "test"),
+            TypeAttribute::new(FunctionType::new(&context, &[], &[]).into()),
+            {
+                let block = Block::new(&[]);
+                block.append_operation(sample::sample(&context, location).into());
+                block.append_operation(func::r#return(&[], location));
+                let region = Region::new();
+                region.append_block(block);
+                region
+            },
+            &[],
+            location,
+        ));
+
+        let result = r#"module {
+  func.func @test() {
+    sample.sample
+    return
+  }
+}
+"#;
+        assert_eq!(module.as_operation().to_string(), result);
     }
 }
